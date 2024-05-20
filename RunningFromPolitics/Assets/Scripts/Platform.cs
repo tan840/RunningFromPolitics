560101ec2c6d1f787b0df51b1247890b1b84710d
Tanvir Ahmed <42252274+tan840@users.noreply.github.com>
2024-05-20 22:14:12 +0600

    Tested Optimization Techniques

--- RunningFromPolitics/Assets/Scripts/Platform.cs
+++ RunningFromPolitics/Assets/Scripts/Platform.cs
@@ -2,26 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Platform : MonoBehaviour
 {
-    //[SerializeField] Transform[] m_Spawnpoints;
-    //LevelPoolManager m_levelPoolManager;
-    //GameManager m_GameManager;
-    //List<GameObject> m_Obstacle;
-    //private void Start()
-    //{
-        //m_levelPoolManager = LevelPoolManager.Instance;
-        //m_GameManager = GameManager.Instance;   
-    //}
-    //public void InitializeObstacle()
-    //{
-    //    ResetObstacle();
-    //}
-    //void ResetObstacle()
-    //{
-    //    foreach (var obstacle in m_Obstacle)
-    //    {
-    //        obstacle.transform.localPosition = Vector3.zero;
-    //    }
-    //}
+
 }
